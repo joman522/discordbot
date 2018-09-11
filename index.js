e7c91053f542fc7fc54af43e6449d7a21859ef47
joman522 <woidbua2@gmail.com>
2018-09-11 18:33:14 +0200

    removed point system(temp)

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const commando = require('discord.js-commando');
 const bot = new commando.Client();
 const token = process.env.token;
-let points = require('./points.json');
 
 bot.registry.registerGroup('random', 'Zufall');
 bot.registry.registerGroup('pics', 'Memes');
 bot.registry.registerGroup('admin', 'Admin');
-bot.registry.registerGroup('points', 'Punktevergabe');
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + "/commands");
 
