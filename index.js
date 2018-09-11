2ad4a578ba42cf8bc57758ac71a8dcfe7e5ea6ae
joman522 <woidbua2@gmail.com>
2018-09-11 18:27:53 +0200

    removed token

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const { prefix, token } = require('./config.json');
 const commando = require('discord.js-commando');
 const bot = new commando.Client();
+const token = process.env.token;
 let points = require('./points.json');
 
 bot.registry.registerGroup('random', 'Zufall');
@@ -10,4 +10,4 @@ bot.registry.registerGroup('points', 'Punktevergabe');
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + "/commands");
 
-bot.login(token);
+bot.login(token).catch(err => console.log(err));
